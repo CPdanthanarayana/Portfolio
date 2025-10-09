42e6814e66df682e56bf948f8d4a318c239a81a5
chamath <dcpoorna887@gmail.com>
2025-10-09 21:48:37 +0530

    setup homepage add some frame motions

--- components/Header.jsx
+++ components/Header.jsx
@@ -16,7 +16,7 @@ const Header = () => {
         </Link>
 
         {/* desktop nav & hire me button*/}
-        <div className="hidden xl:flex items-center gap-8">
+        <div className="hidden lg:flex items-center gap-8">
           <Nav />
           <Link href="/contact">
             <Button>Hire Me</Button>
@@ -24,7 +24,7 @@ const Header = () => {
         </div>
 
         {/* mobile nav */}
-        <div className="xl:hidden">
+        <div className="lg:hidden">
           <MobileNav />
         </div>
       </div>
